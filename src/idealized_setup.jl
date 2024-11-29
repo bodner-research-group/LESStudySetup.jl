a6167f5b7883563c404ad04912cb979997761027
Simone Silvestri <silvestri.simone0@gmail.com>
2024-11-29 15:48:45 +0100

    default config

--- src/idealized_setup.jl
+++ src/idealized_setup.jl
@@ -116,20 +116,21 @@ function default_experimental_setup!(Δh, Δz)
               Lz = 252,
                Q = 40.0,   # Cooling heat flux in W/m²
               τw = 0.1,    # Wind stress in N/m²
-               # Initial condition
+               θ = 30.0, # Wind stress angle in degrees (0 correspond to zonal wind stress)
+              # Initial condition - frontal values
+             M²₀ = 5e-7,
               T₀ = 20,
               m₀ = 60,
              Δmᶠ = 10,
              N²s = 5e-7,
-             N²T = 1e-4,
-             M²₀ = 5e-7,
-             θ   = 30.0, # Wind stress angle in degrees (0 correspond to zonal wind stress)
-             ΔTᵉ = 0.5,  # Eddy temperature difference
+             N²T = 2e-4,
+             # Eddy forcing
+             ΔTᵉ = 0.1,  # Eddy temperature difference
              Φ   = 0.01, # Barotropic eddy strength
-             a   = 1.2,  # Eddy temperature magnitude
+             a   = 1.0,  # Eddy temperature magnitude
              Lf  = 0.9,  # Size of temperature front (large numbers correspond to steeper fronts)
              σ²  = 1.0)
-
+    
     return nothing
 end
 
